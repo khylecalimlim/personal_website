a985d291b38e54d4f20235a94c19ff019561d3ff
khyle <khylecalimlim@gmail.com>
2026-06-03 22:13:45 -0500

    Revamp home page, center About Me header, update resume PDF

--- my-website-Angular/src/app/home/home.component.ts
+++ my-website-Angular/src/app/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ThemeService } from '../theme.service';
+
+const KONAMI = [
+  'ArrowUp','ArrowUp','ArrowDown','ArrowDown',
+  'ArrowLeft','ArrowRight','ArrowLeft','ArrowRight',
+  'b','a'
+];
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   theme = inject(ThemeService);
+  easterEggActive = signal(false);
+  private keyBuffer: string[] = [];
+
+  @HostListener('window:keydown', ['$event'])
+  onKey(e: KeyboardEvent) {
+    this.keyBuffer.push(e.key);
+    if (this.keyBuffer.length > KONAMI.length) this.keyBuffer.shift();
+    if (this.keyBuffer.join() === KONAMI.join()) {
+      this.easterEggActive.set(true);
+      this.keyBuffer = [];
+    }
+  }
+
+  dismissEasterEgg() {
+    this.easterEggActive.set(false);
+  }
 }
